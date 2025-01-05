71f464cc57aea4be282209e20a02401b123e5615
AryamaanScaler <aryamaan.jain@scaler.com>
2025-01-05 19:46:36 +0530

    user update and nginx conf

---
+++ .platform/confighooks/prebuild/01_prebuild.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+echo "Starting Config Hooks postbuild script..."
+
+cp -rf /var/app/current/nginx/nginx.conf /etc/nginx/
+if [ $? -eq 0 ]; then
+    echo "File copied successfully."
+else
+    echo "Failed to copy file."
+fi
+
+sudo systemctl restart nginx
+echo "postbuild script finished."
+
+exit 0
