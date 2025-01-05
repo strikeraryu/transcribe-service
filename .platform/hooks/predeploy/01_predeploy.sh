57b891523718071434bdd55db0bb8de23f6009fc
AryamaanScaler <aryamaan.jain@scaler.com>
2025-01-05 21:25:53 +0530

    tmp dir

---
+++ .platform/hooks/predeploy/01_predeploy.sh
@@ -0,0 +1,5 @@
+#!bin/sh
+
+cd /var/app/staging
+sudo mkdir -p ./tmp
+sudo chmod 777 ./tmp
--- scripts/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Create a temporary directory and give it access to all users
-sudo mkdir -p ./tmp
-sudo chmod 777 ./tmp
