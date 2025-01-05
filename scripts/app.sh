3a2f7377b13597444e24b1662c173bffb46ea94a
AryamaanScaler <aryamaan.jain@scaler.com>
2025-01-05 13:34:37 +0530

    scripts update

--- scripts/app.sh
+++ scripts/app.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+source ./setup.sh
 poetry run gunicorn --config gunicorn_config.py service:app;
--- scripts/consumer.sh
+++ scripts/consumer.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+source ./setup.sh
 poetry run celery -A service.celery worker --loglevel=INFO --pool threads 
