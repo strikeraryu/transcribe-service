af63ab8045debbabe9a67f30b107b34be240f61c
AryamaanScaler <aryamaan.jain@scaler.com>
2025-01-09 17:20:14 +0530

    change: retry count and mock

--- tasks/transcribe_task.py
+++ tasks/transcribe_task.py
@@ -8,7 +8,7 @@ from models import db
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-@celery_app.task(bind=True, acks_late=True, autoretry_for=(Exception,), max_retries=1, default_retry_delay=10)
+@celery_app.task(bind=True, acks_late=True, autoretry_for=(Exception,), max_retries=5, default_retry_delay=10)
 def transcribe_task(self, task_id):
     logger.debug(f'Starting transcribe_task for task_id: {task_id}')
     task = Task.query.get(task_id)
